75b330e135c9072dca6708a14b23e48eed1b1f81
LeartaPllana <leartapllana9@gmail.com>
2025-02-28 20:38:14 +0100

    UserInfo model and migration and relation  compleated

---
+++ app/Models/UserInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+class UserInfo extends Model
+{
+    /**
+     *      @var array<string>
+
+     */
+
+    protected $fillable = [
+        'user_id',
+        'address',
+        'tel',
+        'about',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
