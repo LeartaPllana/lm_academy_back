d94714a3a6781d402e2afcb260e6c01140948389
LeartaPllana <leartapllana9@gmail.com>
2025-02-28 21:14:40 +0100

    Created user list items pivot table and related relationships many-to-many

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\UserInfo;
+use App\Models\UserList;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use App\Models\UserInfo;
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -52,5 +54,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function UserInfo(){
+        return $this->hasOne(UserInfo::class, 'user_id', 'id');
+    }
+
+    public function lists(){
+        return $this->belongsToMany(UserList::class, 'user_list_items','user_id','list_id')->withTimestamps();
     }
 }
