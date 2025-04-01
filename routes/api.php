a790b11ae6fa2e5e9ad3ad889919041680a628af
LeartaPllana <leartapllana9@gmail.com>
2025-04-01 17:08:52 +0200

    Enhance API routes and add test mail functionality
    - Refactored API routes for better readability and organization.
    - Introduced a new route for sending test emails, utilizing the TestMail Mailable class.
    - Added error handling for the email sending process.
    - Updated web routes to include a view for the test mail functionality.

--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,51 @@
 
 <?php
 
-use App\Http\Controllers\AuthController;
-use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
+    use App\Http\Controllers\AuthController;
+    use App\Mail\TestMail;
+    use GuzzleHttp\Middleware;
+    use Illuminate\Support\Facades\Mail;
+    use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->prefix("auth")->middleware('api')->group(function()  {
-    Route::post('login','login')->name('auth.login');
-    Route::post('register','register')->name('auth.register');
-    Route::post('refresh','refresh')->name('auth.refresh');
-    Route::middleware('jwt.auth.token',)->group(function(){
-        Route::post('logout', 'logout')->name('auth.logout');
-        Route::get('user-profile','userProfile')->name('auth.user-profile');
-        Route::post('send-registration-invite', 'sendRegistrationInvite')->name('auth.sendRegistrationInvite');
+    Route::controller(AuthController::class)->prefix("auth")->middleware('api')->group(function () {
+        Route::post('login', 'login')->name('auth.login');
+        Route::post('register', 'register')->name('auth.register');
+        Route::post('refresh', 'refresh')->name('auth.refresh');
+
+        Route::middleware('jwt.auth.token', )->group(function () {
+            Route::post('logout', 'logout')->name('auth.logout');
+            Route::get('user-profile', 'userProfile')->name('auth.user-profile');
+            Route::post('send-registration-invite', 'sendRegistrationInvite')->name('auth.sendRegistrationInvite');
+
+        });
 
     });
 
+    // Route::get('test-api-endpoint', function(){
+    //     return response()->json(['message'=> 'API endpoint is working']);
+    // });
+
+    Route::post('test-mail-sent', function (Request $request) {
+        try {
+            $mailData = [
+                'title'  => 'Email Title',
+                'message' => 'This is a test e-mail directed to only students of Lutfi Musiqi High School.',
+                'session_title'  => $request->session_title
+            ];
+           
+            Mail::to('leartapllana9@gmail.com')->send(new TestMail($mailData));
+            return response()->json('success');
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false, 
+                'error' => [
+                    'code' => $e->getCode(), 
+                    'message' => $e->getMessage(),
+                    'type' => class_basename($e)
+                ]
+            ], 500);
+        }
+
 });
-
-
-// Route::get('test-api-endpoint', function(){
-//     return response()->json(['message'=> 'API endpoint is working']);
-// });
