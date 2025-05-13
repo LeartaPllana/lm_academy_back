03a959348c66db9189d83060bf22f2fe237c8391
LeartaPllana <leartapllana9@gmail.com>
2025-05-13 14:47:25 +0200

    Refactor AuthController and RefreshToken middleware for improved functionality and error handling
    - Updated AuthController to use Auth facade for user retrieval.
    - Enhanced validation logic in the register method and improved response structure.
    - Refined userProfile and logout methods for better clarity and error handling.
    - Improved RefreshToken middleware to handle authentication exceptions more effectively.
    - Adjusted JWT configuration to set token expiration time to 1 minute for testing purposes.

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -1,15 +1,15 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
-
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 class RefreshToken extends BaseMiddleware
 {
     /**
@@ -21,17 +21,17 @@ class RefreshToken extends BaseMiddleware
     {
         try {
             $this->checkForToken($request);
-            if ($request->user = JWTAuth::parseToken()->authenticate()) {
+
+            if($user = JWTAuth::parseToken()->authenticate()){
                 return $next($request);
             }
-            throw new AuthenticationException('Unathorized', []);
+
+            throw new AuthenticationException('Unauthorized', []);
 
         } catch (TokenExpiredException $e) {
-            throw new HttpResponseException(response()->json([
-                "message" => 'token expired'
-            ],401));
+            throw new HttpResponseException(response()->json(['message' => 'token expired'], 401));
         } catch (\Exception $e) {
-            throw new AuthenticationException('Unathorized', []);
+            throw new AuthenticationException('Unauthorized', []);
         }
     }
 }
